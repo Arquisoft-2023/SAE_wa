1580379d6bd2d0120a150bbce6b22ab454e0db70
julozanoa <julozanoa@unal.edu.co>
2023-06-03 19:40:18 -0500

    middleware deleted

--- vite.config.ts
+++ vite.config.ts
@@ -10,20 +10,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: PORT,
-    host: URI,
-    middleware: [
-      (req, res, next) => {
-        // Aquí puedes agregar tus reglas de acceso personalizadas
-        // Verificar la solicitud (req) y responder (res) según sea necesario
-
-        // Ejemplo de regla de acceso que permite todas las solicitudes GET
-        if (req.method === 'GET') {
-          next(); // Permite la solicitud continuar
-        } else {
-          res.status(403).end('Acceso denegado'); // Responde con un estado 403 para otras solicitudes
-        }
-      },
-    ],
+    host: URI
   },
   base: "/sae"
 });
