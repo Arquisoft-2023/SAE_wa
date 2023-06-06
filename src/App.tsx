7f50ffae6b1e5de87bbf5fc1e1ed69728b9d6c46
Luis Mendez <81998928+LuisMendez-ING@users.noreply.github.com>
2023-06-06 05:35:39 -0500

    Cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import "./styles/app.css";
 import SaeRoutes from "./routers/SaeRoutes";
+import userStore from "./state/zustand";
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
+  const usuarioSesion = userStore((state) => state.token);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (usuarioSesion) {
+        alert("Verificación Tokens");
+      }
+    }, 60000);
+
+    return () => {
+      if (usuarioSesion === null) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [usuarioSesion]);
   return (
     <>
       <BrowserRouter basename="/sae">
